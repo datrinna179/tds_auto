c7b9b0d8175e97c3d945046556651c7f7fc50c36
Nguyễn Đồng Minh Tiến <135971914+Amilytv@users.noreply.github.com>
2023-08-14 03:44:56 +0000

    thay cookie file muiltithreading đg fix,chưa chạy đc cho code bên file pro 5 vào class muiltithreading để chạy đa luồng 	new file:   __pycache__/pro5_tool_tds.cpython-310.pyc 	modified:   cookie.txt 	new file:   muiltithreading.py 	modified:   pro5_tool_tds.py

---
+++ muiltithreading.py
@@ -0,0 +1,65 @@
+from pro5_tool_tds import *
+import os
+import threading
+import time
+import subprocess
+import pynput
+
+try:
+    from pynput import keyboard
+except:
+    os.system("pip install pynput")
+
+from pynput import keyboard
+tools = [muiltithreading('Thread 1'), muiltithreading('Thread 2'), muiltithreading('Thread 3')]
+listen = keyboard.Listener
+
+def execute_python_file(profile, sleepTime, file_path):
+    if(profile.IsStop):
+        print("{} Stop\n" .format(profile.Name))
+        return
+    time.sleep(sleepTime)
+    if(profile.IsStop):
+        print("{} Stop\n" .format(profile.Name))
+        return
+    #dung tool sau khi nhan phim tat 
+    totalRunningThread = any(x.IsStop == False for x in tools)
+    print("Total: {}\n".format(totalRunningThread))
+    if(totalRunningThread == False):
+        listen.stop()
+    #chạy file pro5_tool_tds
+    try:
+        completed_process = subprocess.run(['python', file_path], capture_output=True, text=True)
+        if completed_process.returncode == 0:
+            print("Execution successful.")
+            print(f"{profile.Name} is running")
+            print("Output:")
+            print(completed_process.stdout)
+        else:
+            print(f"Error: Failed to execute '{file_path}'.")
+            print("Error output:")
+            print(completed_process.stderr)
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' does not exist.")
+
+file_path = '/workspaces/tds_auto/pro5_tool_tds.py'
+    
+#nhan phim de dung tool
+def on_press(key): # key: phim nhan
+    vk = key.vk if hasattr(key, 'vk') else key.value.vk 
+    print('vk = ',vk)
+    if(vk == None):
+        return
+    index = vk - 48
+    if(index >=0 and index < len(tools) and tools[index].IsStop == False):
+        print("Doing Stop: {}".format(tools[index].Name))
+        tools[index].IsStop = True
+#muiltithreading
+if __name__ == "__main__":
+    for item in tools:
+        p = threading.Thread(target=execute_python_file, args=(item, 3, file_path, ))
+        p.start()
+        
+    with keyboard.Listener(on_press=on_press) as listener:
+        listen = listener
+        listener.join() 
